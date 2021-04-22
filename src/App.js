555810e4deec2f03ab92e5fbe63beab4fb0eb20b
Jere Pesälä <j.pesala@protonmail.com>
2021-04-22 10:03:23 +0300

    Code together project start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+
+          <div className="grid">
+              <div className="logo">
+                  Go Oulu
+              </div>
+
+              <div className="buttons">
+                  <button> Rekisteröidy </button>
+                  <button> Kirjaudu </button>
+                  <button> Jatka kirjautumatta</button>
+              </div>
+          </div>
+
+      </div>
   );
 }
 
