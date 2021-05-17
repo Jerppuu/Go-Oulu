a52ae91e8ed8d9d803eb96c857578c50068478c1
Jere Pesälä <j.pesala@protonmail.com>
2021-05-17 23:10:15 +0300

    Basics of Google maps api added, functional map rendering

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import map from "./extras/map.png";
+import { GoogleMap, useLoadScript, Marker, InfoWindow,} from "@react-google-maps/api";
+import mapStyles from "./mapStyles";
+
 
 function Map(props) {
+    const libraries = ["places"];
+    const mapContainerStyle = {
+        width: "100vw",
+        height: "100vh"
+    };
+    const center = {
+        lat: 65.012093,
+        lng: 25.465076,
+    };
+    const options = {
+        styles: mapStyles,
+        disableDefaultUI: true,
+    }
+
+    const {isLoaded, loadError} = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, libraries,});
+
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading maps"
+
     return (
         <div>
             <meta
                 name='viewport'
                 content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
             />
-            <div className="mapGrid">
-                <img src={map} alt="map" style={{maxWidth: "100%", objectFit: "cover", gridRow: "2"}} />
-            </div>
+
+                <GoogleMap mapContainerStyle={mapContainerStyle} zoom={15} center={center} options={options} ></GoogleMap>
+
+
 
             <div className="footer">
                 <div className="footerGrid">
