9e334465d0935dd61e0cb65fdac27c7afeec694b
Jere Pesälä <j.pesala@protonmail.com>
2021-05-18 00:35:53 +0300

    Marker function added, with two expample markers

--- src/Map.js
+++ src/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import { GoogleMap, useLoadScript, Marker, InfoWindow,} from "@react-google-maps/api";
 import mapStyles from "./mapStyles";
 
@@ -18,6 +18,18 @@ function Map(props) {
         disableDefaultUI: true,
     }
 
+    const mapRef = useRef();
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    }, [])
+
+    const markers = [
+        {lat: 65.012804, lng: 25.466924, name: "Kauppuri 5"},
+        {lat: 65.010717, lng: 25.469784, name: "45 Special"},
+    ];
+
+    const [selected, setSelected] = useState(null);
+
     const {isLoaded, loadError} = useLoadScript({googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, libraries,});
 
     if (loadError) return "Error loading maps";
@@ -29,9 +41,33 @@ function Map(props) {
                 name='viewport'
                 content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
             />
+                <GoogleMap mapContainerStyle={mapContainerStyle} zoom={15} center={center} options={options}>
 
-                <GoogleMap mapContainerStyle={mapContainerStyle} zoom={15} center={center} options={options} ></GoogleMap>
+                    {markers.map((marker) => (
+                    <Marker
+                        key={marker.name}
+                        position={{lat: marker.lat, lng: marker.lng}}
+                        //TODO : add marker image
+                        /*icon={{
+                            url: "./imagehere",
+                            scaledSize: new window.google.maps.size(30,30),
+                            origin: new window.google.maps.Point(0,0),
+                            anchor: new window.google.maps.Point(15,15),
 
+                        }}*/
+                        onClick={() => setSelected(marker)}
+                    />
+                    ))}
+
+                    //Todo: add more information about places
+                    {selected ? (<InfoWindow position={{lat: selected.lat, lng: selected.lng}} onCloseClick={() => {setSelected(null)}}>
+                        <div>
+                            <h2>Kohteen nimi</h2>
+                            <p>{selected.name}</p>
+                        </div>
+                    </InfoWindow>) : null}
+
+                    </GoogleMap>
 
 
             <div className="footer">
